c38b5810b2e91614003d1da453516bcd412d6dde
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-22 22:07:46 +0300

    feat: complete finalTask module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,22 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+        List<String> projectsDev = new ArrayList<>();
+
+        for(Map.Entry<String, Set<String>> item : projects.entrySet()){
+            if (item.getValue().contains(developer)) {
+                projectsDev.add(item.getKey());
+            }
+        }
+
+        projectsDev.sort(Comparator.comparingInt(String::length).reversed()
+                .thenComparing(Comparator.reverseOrder()));
+
+        return projectsDev;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+
+        for(Map.Entry<String, Integer> item : sourceMap.entrySet()){
+            int keySize = item.getKey().length();
+            if (!resultMap.containsKey(keySize)) {
+                Set<String> values = new HashSet<>();
+                values.add(item.getKey());
+                resultMap.putIfAbsent(keySize, values);
+            } else {
+                Set<String> oldValues = resultMap.get(keySize);
+                oldValues.add(item.getKey());
+                resultMap.replace(keySize, oldValues);
+            }
+        }
+
+        return resultMap;
     }
 }
